c8070b9ac1358902ca3fee7923211f0c32a0f69f
millxore <millcore18@gmail.com>
2026-07-11 14:30:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 // MIDDLEWARE - Fix for CORS and JSON parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // Add CORS headers
 app.use((req, res, next) => {
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 // Serve HTML pages
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'sender.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/receiver', (req, res) => {
